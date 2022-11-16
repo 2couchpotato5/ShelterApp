e01b83b7e4221700dc88e245120741f9290fc6b1
leila <leila.azimova2003@gmail.com>
2022-11-15 20:31:10 -0800

    Finished login page and half of the user page

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,37 +2,12 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) {
-        ImageIcon image = new ImageIcon(new ImageIcon("src/main/logo.png").getImage().getScaledInstance(
-                250, 250, Image.SCALE_DEFAULT));
 
-
-        JLabel label = new JLabel();
-        label.setText("Welcome to the Shelter App");
-        label.setIcon(image);
-        label.setHorizontalTextPosition(JLabel.CENTER);
-        label.setVerticalTextPosition(JLabel.TOP);
-        label.setForeground(new Color(0,102,0));
-        label.setFont(new Font("MV Boli", Font.PLAIN, 35));
-        label.setIconTextGap(0);
-        label.setVerticalAlignment(JLabel.TOP);
-        label.setHorizontalAlignment(JLabel.CENTER);
-        label.setBounds(225,0,500,300);
-
-
-        JFrame frame = new JFrame(); //creates the frame
-        frame.setTitle("Shelter App");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1000,700); //sets dimensioon
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(new Color(204,229,250));
-        frame.add(label);
-        frame.setLayout(null);
-        //frame.pack() need to add all components and then pack
-
-//
+        new ShelterAppGui();
 //        try {
 //            new ShelterApp();
 //        } catch (FileNotFoundException e) {
---
+++ src/main/ui/ShelterAppGui.java
@@ -0,0 +1,280 @@
+package ui;
+
+import model.Clothes;
+import model.Shelter;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ShelterAppGui extends JFrame implements ActionListener {
+    private static final String JSON_STORE = "./data/shelter.json";
+    private JsonWriter jsonWriter;
+    private JsonReader jsonReader;
+    private JFrame login;
+    private JFrame user;
+    private JFrame sponsor;
+    private JLabel login1;
+    private JLabel login2;
+    private JButton userB;
+    private JButton sponsorB;
+    private JButton list;
+    private JButton request;
+    private JButton save;
+    private JButton load;
+    private JButton seeRequest;
+    private JButton donation;
+    private JButton select;
+    private JPanel panel;
+    private JPanel spaceArea;
+    private JPanel buttonArea;
+    private Shelter shelter;
+    private DefaultListModel clothes;
+    private DefaultListModel furniture;
+    private DefaultListModel requests;
+
+
+
+
+
+    public ShelterAppGui() {
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
+        initLoginPage();
+        shelter = new Shelter();
+        shelter.addFurniture("chair");
+        shelter.addClothes(new Clothes("coat", "black", 45));
+        shelter.addRequest("chair");
+        shelter.fund(500);
+        shelter.addRequest("blouse");
+        shelter.addClothes(new Clothes("skirt", "white", 36));
+        shelter.addFurniture("table");
+    }
+
+    public void initLoginPage() {
+        frameInit();
+        loginO();
+        loginT();
+        gridInit();
+
+    }
+
+    public void loginO() {
+        ImageIcon image = new ImageIcon(new ImageIcon("src/main/logo.png").getImage().getScaledInstance(
+                250, 250, Image.SCALE_DEFAULT));
+        login1 = new JLabel();
+        login1.setText("Welcome to the Shelter App");
+        login1.setIcon(image);
+        login1.setHorizontalTextPosition(JLabel.CENTER);
+        login1.setVerticalTextPosition(JLabel.TOP);
+        login1.setForeground(new Color(0, 102, 0));
+        login1.setFont(new Font("MV Boli", Font.PLAIN, 35));
+        login1.setIconTextGap(0);
+        login1.setVerticalAlignment(JLabel.TOP);
+        login1.setHorizontalAlignment(JLabel.CENTER);
+
+        panel.add(login1);
+    }
+
+    public void loginT() {
+        login2 = new JLabel();
+        login2.setPreferredSize(new Dimension(500, 50));
+        login2.setText("Choose how you want to login");
+        login2.setFont(new Font("MV Boli", Font.BOLD, 22));
+        login2.setHorizontalAlignment(JLabel.CENTER);
+
+        panel.add(login2);
+    }
+
+    public void gridInit() {
+
+        login.setLayout(new GridLayout(0,1));
+        userB = new JButton("User");
+        userB.addActionListener(this);
+        userB.setPreferredSize(new Dimension(250, 100));
+        panel.add(userB);
+        sponsorB = new JButton("Sponsor");
+        sponsorB.addActionListener(this);
+        sponsorB.setPreferredSize(new Dimension(250, 100));
+        panel.add(sponsorB);
+        login.setContentPane(panel);
+        login.setVisible(true);
+    }
+
+    public void frameInit() {
+        login = new JFrame();
+        login.setTitle("Shelter App");
+        login.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        login.setSize(1000, 700); //sets dimensioon
+        login.setResizable(false);
+        panel = new JPanel(new FlowLayout(FlowLayout.CENTER,500,10));
+        panel.setBackground(new Color(204, 229, 250));
+
+    }
+
+    public void userPage() {
+        user = new JFrame();
+        user.setLayout(new BorderLayout());
+        user.setTitle("User Page");
+        user.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        user.setSize(1000, 700); //sets dimensioon
+        user.setResizable(false);
+        spaceArea = new JPanel();
+        spaceArea.setLayout(new FlowLayout());
+        spaceArea.setSize(new Dimension(0, 0));
+        user.setVisible(true);
+        initButtons();
+    }
+
+    private void initButtons() {
+        list = new JButton("Available items");
+        list.addActionListener(this);
+        request = new JButton("Make request");
+        request.addActionListener(this);
+        save = new JButton("Save changes");
+        save.addActionListener(this);
+        load = new JButton("Load changes");
+        load.addActionListener(this);
+        select = new JButton("Make Selection");
+        select.addActionListener(this);
+        buttonArea = new JPanel();
+        buttonArea.setLayout(new GridLayout(0,1));
+        buttonArea.setSize(new Dimension(0, 0));
+        buttonArea.add(list);
+        buttonArea.add(request);
+        buttonArea.add(save);
+        buttonArea.add(load);
+        user.add(buttonArea, BorderLayout.WEST);
+
+    }
+
+    private void printClothes() {
+        clothes = new DefaultListModel();
+        JList lcl = new JList(clothes);
+        lcl.setFont(new Font("MV Boli", Font.ITALIC, 15));
+        JScrollPane scrollPane = new JScrollPane(lcl);
+        String title;
+        title = "Shelter has the following clothes available:";
+        clothes.addElement(title.toUpperCase());
+        for (Clothes c : shelter.getClothes()) {
+            String k;
+            k = (c.getName().substring(0,1).toUpperCase() + c.getName().substring(1).toLowerCase()
+                    + " in " + c.getColor() + " color and in size " + c.getSize());
+            clothes.addElement(k);
+        }
+        if (clothes.getSize() < 1) {
+            clothes.removeAllElements();
+            clothes.addElement("There are no available clothes at this moment");
+        }
+        spaceArea.add(scrollPane);
+        user.add(spaceArea, BorderLayout.CENTER);
+        spaceArea.revalidate();
+        spaceArea.repaint();
+        buttonArea.add(select);
+    }
+
+    public void sponsorPage() {
+        sponsor = new JFrame();
+        setLayout(new BorderLayout());
+        sponsor.setTitle("Sponsor Page");
+        sponsor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sponsor.setSize(1000, 700); //sets dimensioon
+        sponsor.setResizable(false);
+        sponsor.setVisible(true);
+        initButtonS();
+    }
+
+    private void initButtonS() {
+        list = new JButton("Available items");
+        list.addActionListener(this);
+        seeRequest = new JButton("Current request");
+        seeRequest.addActionListener(this);
+        donation = new JButton("Make a donation");
+        donation.addActionListener(this);
+        save = new JButton("Save changes");
+        save.addActionListener(this);
+        load = new JButton("Load changes");
+        load.addActionListener(this);
+        JPanel buttonArea = new JPanel();
+        buttonArea.setLayout(new GridLayout(0,1));
+        buttonArea.setSize(new Dimension(0, 0));
+        buttonArea.add(list);
+        buttonArea.add(seeRequest);
+        buttonArea.add(donation);
+        buttonArea.add(save);
+        buttonArea.add(load);
+        sponsor.add(buttonArea, BorderLayout.WEST);
+
+    }
+
+    private void makeRequests() {
+    }
+
+    private void makeSelection() {
+    }
+
+    private void printRequests() {
+    }
+
+    private void makeDonation() {
+    }
+
+
+
+    // EFFECTS: saves the changes
+    private void saveShelter() {
+        try {
+            jsonWriter.open();
+            jsonWriter.write(shelter);
+            jsonWriter.close();
+            JOptionPane.showMessageDialog(null, "Saved changes to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Unable to write to file: " + JSON_STORE,
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: loads shelter from file
+    private void loadShelter() {
+        try {
+            shelter = jsonReader.read();
+            JOptionPane.showMessageDialog(null,"Loaded shelter changes from " + JSON_STORE);
+            load.setEnabled(false);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Unable to read from file: " + JSON_STORE,
+                    "File error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == userB) {
+            login.setVisible(false);
+            userPage();
+        } else if (e.getSource() == sponsorB) {
+            login.setVisible(false);
+            sponsorPage();
+        } else if (e.getSource() == save) {
+            saveShelter();
+        } else if (e.getSource() == load) {
+            loadShelter();
+        } else if (e.getSource() == list) {
+            printClothes();
+        } else if (e.getSource() == request) {
+            makeRequests();
+        } else if (e.getSource() == seeRequest) {
+            printRequests();
+        }  else if (e.getSource() == donation) {
+            makeDonation();
+        } else if (e.getSource() == select) {
+            makeSelection();
+        }
+
+    }
+}
